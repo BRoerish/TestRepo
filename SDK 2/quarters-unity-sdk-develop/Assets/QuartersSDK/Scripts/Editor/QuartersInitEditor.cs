786cc46bed970fa65acaa6b76ba4feff95f6695d
Brad Roerish <brad@poq.gg>
2022-05-17 09:53:58 -0600

    Test commit

---
+++ SDK 2/quarters-unity-sdk-develop/Assets/QuartersSDK/Scripts/Editor/QuartersInitEditor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using QuartersSDK;
+
+[CustomEditor(typeof(QuartersInit))]
+public class QuartersInitEditor : UnityEditor.Editor {
+
+
+    public override void OnInspectorGUI() {
+        
+        QuartersInit quartersInit = (QuartersInit)target;
+
+        EditorGUILayout.LabelField($"Quarters Unity SDK - Version {QuartersInit.SDK_VERSION}");
+        if (GUILayout.Button("Open Dashboard")) {
+            Application.OpenURL(quartersInit.DASHBOARD_URL);
+        }
+        EditorGUILayout.Space();
+        base.DrawDefaultInspector();
+
+
+
+    }
+
+
+}
+
